e714392a8848008e093840b8b5de7d0ac6400fd9
Howard.Zuo <leftstick@qq.com>
2016-05-19 17:56:14 +0800

    add deploy config

---
+++ webpack.config.prod.js
@@ -0,0 +1,60 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './js/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].bundle.js',
+        chunkFilename: '[id].bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'babel?{"presets":["es2015"]}!vue',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel?{"presets":["es2015"]}',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            },
+            {
+                test: /\.(png)\w*/,
+                loader: 'file'
+            }
+        ]
+    },
+    resolve: {
+        root: [
+            path.resolve(__dirname)
+        ],
+        extensions: [
+            '',
+            '.js',
+            '.vue'
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            inject: 'body',
+            template: 'index.html_vm',
+            favicon: 'img/favicon.ico',
+            hash: false
+        })
+    ]
+};
