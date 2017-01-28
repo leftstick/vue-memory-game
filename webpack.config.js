61d28e0b28c328032e95e49cb7b2b62585e751e1
Howard.Zuo <leftstick@qq.com>
2017-01-28 19:11:45 +0800

    upgrade to webpack=>2.2.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,18 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const {resolve, join} = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
 
-module.exports = {
-    entry: {
-        index: './js/index.js'
-    },
+module.exports = merge(common, {
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].[hash].bundle.js'
+        path: resolve(__dirname, 'build')
     },
-    debug: true,
     devtool: '#eval-source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.vue$/,
-                loader: 'vue',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel?{"presets":["es2015"],"plugins": ["transform-object-rest-spread"]}',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                loader: 'style!css'
-            },
-            {
-                test: /\.(png)$/,
-                loader: 'file'
-            }
-        ]
-    },
-    vue: {
-        loaders: {
-            js: 'babel?{"presets":["es2015"],"plugins": ["transform-object-rest-spread"]}',
-            css: 'vue-style!css'
-        }
-    },
-    resolve: {
-        root: [
-            path.resolve(__dirname),
-            path.resolve(__dirname, 'js')
-        ],
-        extensions: [
-            '',
-            '.js',
-            '.vue'
-        ],
-        alias: {
-            vue: 'vue/dist/vue.min.js'
-        }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            inject: 'body',
-            template: 'index.html_vm',
-            favicon: 'img/favicon.ico',
-            hash: false
-        })
-    ]
-};
+    devServer: {
+        contentBase: join(__dirname, 'build'),
+        compress: false,
+        port: 8080,
+        host: '0.0.0.0',
+        hot: true,
+        inline: true
+    }
+});
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,56 +1,11 @@
-var webpack = require('webpack');
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const {resolve} = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+const webpack = require('webpack');
 
-module.exports = {
-    entry: {
-        index: './js/index.js'
-    },
+module.exports = merge(common, {
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[hash].bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.vue$/,
-                loader: 'vue',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel?{"presets":["es2015"],"plugins": ["transform-object-rest-spread"]}',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                loader: 'style!css'
-            },
-            {
-                test: /\.(png)$/,
-                loader: 'file'
-            }
-        ]
-    },
-    vue: {
-        loaders: {
-            js: 'babel?{"presets":["es2015"],"plugins": ["transform-object-rest-spread"]}',
-            css: 'vue-style!css'
-        }
-    },
-    resolve: {
-        root: [
-            path.resolve(__dirname),
-            path.resolve(__dirname, 'js')
-        ],
-        extensions: [
-            '',
-            '.js',
-            '.vue'
-        ],
-        alias: {
-            vue: 'vue/dist/vue.min.js'
-        }
+        path: resolve(__dirname, 'dist')
     },
     plugins: [
         new webpack.DefinePlugin({
@@ -58,13 +13,10 @@ module.exports = {
                 NODE_ENV: '"production"'
             }
         }),
-
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            inject: 'body',
-            template: 'index.html_vm',
-            favicon: 'img/favicon.ico',
-            hash: false
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
         })
     ]
-};
+});
