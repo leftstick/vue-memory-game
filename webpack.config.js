2372091b8fefe499a3d9c1e652f48f86e92186d0
Howard.Zuo <leftstick@qq.com>
2020-07-20 17:17:21 +0800

    chore: upgrade deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const { resolve, join } = require('path')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 const webpack = require('webpack')
 
