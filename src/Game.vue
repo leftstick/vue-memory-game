fdad516eda762c0b339a6b160c746ed8ef67016b
Howard.Zuo <leftstick@qq.com>
2021-06-03 15:50:34 +0800

    fix: move global style into Game.vue

--- src/Game.vue
+++ src/Game.vue
@@ -26,6 +26,26 @@ export default defineComponent({
 })
 </script>
 
+<style>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
 <style scoped>
 .game-panel {
   width: 450px;
